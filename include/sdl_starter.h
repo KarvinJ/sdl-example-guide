44f2586d85c6ab9b1ef2347ac4878edceadcba93
Karvin <39208059+ArkhamKnightXD@users.noreply.github.com>
2024-09-18 13:04:44 -0400

    Initial commit

---
+++ include/sdl_starter.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <iostream>
+
+const int SCREEN_WIDTH = 960;
+const int SCREEN_HEIGHT = 544;
+const int FRAME_RATE = 60;
+
+int startSDL(SDL_Window *window, SDL_Renderer *renderer);
+
+void capFrameRate(Uint32 frameStartTime);
